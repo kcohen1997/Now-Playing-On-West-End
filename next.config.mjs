90098aae93a6e0a018b392358156d117e57acb1b
Kyle Cohen <kcohen1997@gmail.com>
2025-05-21 18:59:48 -0700

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
 
-export default nextConfig;
+const nextConfig = {
+  output: 'export', // Enable static export
+  basePath: isProd ? '/<repo-name>' : '', // GitHub Pages expects this
+  assetPrefix: isProd ? '/<repo-name>/' : '', // Needed for CSS/JS assets
+};
+
+module.exports = nextConfig;
