3d56fc8006b4af2e08a22be49579d6318dc8526c
Kyle Cohen <kcohen1997@gmail.com>
2025-10-07 14:30:34 -0700

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "www.londontheatre.co.uk", // Puppeteer scraped images
+      "images.londontheatre.co.uk", // Sometimes used for posters
+      "upload.wikimedia.org", // Wikipedia fallback/default images
+    ],
+  },
+};
 
 export default nextConfig;
